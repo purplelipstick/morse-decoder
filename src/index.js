02b423f556c671154333fecf115fc1751ef76a1a
Elena Ivanova <elenaxivanova@gmail.com>
2020-10-23 11:34:06 +0300

    feat: add decode func

--- src/index.js
+++ src/index.js
@@ -1,46 +1,66 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-':     'a',
+  '-...':   'b',
+  '-.-.':   'c',
+  '-..':    'd',
+  '.':      'e',
+  '..-.':   'f',
+  '--.':    'g',
+  '....':   'h',
+  '..':     'i',
+  '.---':   'j',
+  '-.-':    'k',
+  '.-..':   'l',
+  '--':     'm',
+  '-.':     'n',
+  '---':    'o',
+  '.--.':   'p',
+  '--.-':   'q',
+  '.-.':    'r',
+  '...':    's',
+  '-':      't',
+  '..-':    'u',
+  '...-':   'v',
+  '.--':    'w',
+  '-..-':   'x',
+  '-.--':   'y',
+  '--..':   'z',
+  '.----':  '1',
+  '..---':  '2',
+  '...--':  '3',
+  '....-':  '4',
+  '.....':  '5',
+  '-....':  '6',
+  '--...':  '7',
+  '---..':  '8',
+  '----.':  '9',
+  '-----':  '0',
 };
 
 function decode(expr) {
-    // write your solution here
+let decodedExpression = '';
+const exprLength = expr.length;
+const partLength = 10;
+for (let startingPartIndex = 0; startingPartIndex < exprLength; startingPartIndex+=partLength) {
+  let morseWord = '';
+  const currentExprPart = expr.substr(startingPartIndex, partLength);
+  if (currentExprPart === "**********") {
+    decodedExpression += ' ';
+  } else {
+    for (let startingSubPartIndex = 0; startingSubPartIndex < partLength; startingSubPartIndex+=2) {
+      const subPart = currentExprPart.substr(startingSubPartIndex, 2);
+      if (subPart === '10') {
+        morseWord += '.'
+      } else if (subPart === '11') {
+        morseWord += '-'
+      } // if it's 00, do nothing
+    }
+    decodedExpression += MORSE_TABLE[morseWord];
+  }
+}
+return decodedExpression
 }
 
 module.exports = {
-    decode
+  decode
 }
